4808bc38baf95e4cec1bd26b1fd938f7bcc78ad2
Liza Maykova <elizavetabmstu@yandex.ru>
2020-12-23 17:54:33 +0300

    asd

---
+++ include/SharedPtr.hpp
@@ -0,0 +1,10 @@
+//
+// Created by lizer on 23.12.2020.
+//
+
+#ifndef LAB_03_SHAREDPTR_HPP
+#define LAB_03_SHAREDPTR_HPP
+
+class SharedPtr {};
+
+#endif  // LAB_03_SHAREDPTR_HPP
---
+++ sources/SharedPtr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lizer on 23.12.2020.
+//
+
+#include "SharedPtr.hpp"
